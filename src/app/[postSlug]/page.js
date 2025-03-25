040c723aa1994550bb89d811b528d76191c375be
marienatlandberg <marie.berg2@bergen.kommune.no>
2025-03-25 09:39:45 +0100

    Excercise3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 import BlogHero from '@/components/BlogHero';
-
+import { BLOG_TITLE } from '@/constants';
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = await params;
+
+  const { frontmatter } = await loadBlogPost(postSlug)
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract
+  }
+
+}
+
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
